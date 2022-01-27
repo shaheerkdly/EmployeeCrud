7356e629a7e016f181382bca3adaefbca8ff06fb
Shaheer <shaheer@iocod.com>
2022-01-28 00:08:19 +0530

    Breadcrumb updates

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -13,11 +13,10 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>{{ config('app.name')}}</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="">Home</a></li>
+                    <li class="breadcrumb-item active">Employees</li>
                 </ol>
             </div>
         </div>
